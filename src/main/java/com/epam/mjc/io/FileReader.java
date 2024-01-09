d34ae64fe8aae319de4d9f191d001fbc97f4bd66
nutsa.matchavariani <nutsa.matchavariani@energo-pro.ge>
2024-01-09 17:12:55 +0400

    Task 1 File uploading 3

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -2,47 +2,39 @@ package com.epam.mjc.io;
 
 import java.io.File;
 import java.io.BufferedReader;
-import java.io.IOException;
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile profile = new Profile();
-        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+        try (BufferedReader reader = new BufferedReader( new java.io.FileReader(file) )){
             if (file.exists()) {
                 String line;
 
                 while ((line = reader.readLine()) != null) {
-                    String[] parts = line.split(":");
-                    if (parts.length == 2) {
-                        String key = parts[0].trim();
-                        String value = parts[1].trim();
+                    String key = line.split(": ")[0];
+                    String value = line.split(": ")[1];
 
-                        switch (key) {
-                            case "Name":
-                                profile.setName(value);
-                                break;
-                            case "Age":
-                                profile.setAge(Integer.parseInt(value));
-                                break;
-                            case "Email":
-                                profile.setEmail(value);
-                                break;
-                            case "Phone":
-                                profile.setPhone(Long.parseLong(value));
-                                break;
-                            default:
-                                System.err.println("Line does not match pattern.");
-                        }
+                    switch (key) {
+                        case "Name":
+                            profile.setName(value);
+                            break;
+                        case "Age":
+                            profile.setAge(Integer.parseInt(value));
+                            break;
+                        case "Email":
+                            profile.setEmail(value);
+                            break;
+                        case "Phone":
+                            profile.setPhone(Long.parseLong(value));
+                            break;
+                        default:
+                            System.err.println("Line does not match pattern.");
                     }
                 }
-            } else {
-                System.err.println("File does not exist: " + file.getPath());
             }
-        } catch (IOException e) {
-            e.printStackTrace(); // Handle the exception according to your requirements
-        } catch (NumberFormatException e) {
-            System.err.println("Error parsing number: " + e.getMessage());
+        } catch (Exception e) {
+            System.err.println(e);
         }
         return profile;
     }
